731b47cc8521438ccf2f52bb40fc19724ac3f781
DASPELLER4 <51001942+DASPELLER4@users.noreply.github.com>
2022-01-25 19:56:28 +0100

    Add files via upload

--- include/ncurses.c
+++ include/ncurses.c
@@ -1,5 +1,13 @@
 #ifndef NCURSES_C
 #define NCURSES_C
+#ifdef __unix__
+	#include <sys/types.h>
+	#include <sys/stat.h>
+#else
+	#include <conio.h>
+	#include <sys/types.h>
+	#include <sys/stat.h>
+#endif
 #include <ncurses.h>
 #include <unistd.h>
 #include <stdio.h>
@@ -11,7 +19,7 @@
 
 #define MAX_LINE_LENGTH 80
 
-
+int splitCount=0;
 char project[40] = {0};
 char projectFile[60] = {0};
 
@@ -26,18 +34,21 @@ char **splitBySpace(char* in){
 	char buffer[14];
 	int j = 0;
 	int x = 0;
+	splitCount=0;
 	for(int i = 0; i < strlen(in); i++)
 		ret[i] = malloc(sizeof(char*));
 	for(int i = 0; in[i]; i++){
 		if(in[i+1] == '\0'){
 			buffer[j++] = in[i];
 			buffer[j] = 0;
+			splitCount++;
 			strcpy(ret[x++], buffer);
 			j = 0;
 		} else if(in[i] != ' '){
 			buffer[j++] = in[i];
 		} else {
 			buffer[j] = 0;
+			splitCount++;
 			strcpy(ret[x++], buffer);
 			j = 0;
 		}
@@ -100,7 +111,7 @@ int genAllIncludes(){
         char str[1024] = {0};
 	strcat(str, "#include \"tfuncs.h\"\n");
         while ((files = readdir(dir)) != NULL){
-                if(files->d_name[0] != '.'){
+                if(files->d_name[0] != '.' && strcmp(files->d_name, "allIncludes.h")){
 			strcat(str, "#include \"");
 			strcat(str, files->d_name);
 			strcat(str, "\"\n");
@@ -115,7 +126,68 @@ int genAllIncludes(){
         closedir(dir);
 }
 
-int main(){
+int genCFile(){
+        struct dirent *files;
+	char topDir[30] = {0};
+	strcat(topDir, project);
+	strcat(topDir, "/scripts");
+	char CFile[40] = {0};
+	strcat(CFile, project);
+	strcat(CFile, "/project.c");
+        DIR *dir = opendir(topDir);
+        if (dir == NULL){
+                return 0;
+        }
+        char str[1024] = {0};
+	int x = 0;
+        while ((files = readdir(dir)) != NULL){
+                if(files->d_name[0] != '.' && strcmp(files->d_name, "allIncludes.h") && strcmp(files->d_name, "tfuncs.h")){
+			x++;
+                }
+        }
+	strcat(str, "#include <ncurses.h>\n#include \"scripts/allIncludes.h\"\n#include \"libs/object.h\"\n#include \"libs/render.h\"\n#include \"libs/scene.h\"\nint main(){\nfor(int x = 0; x < 100; x++)\n\tsleeps[x] = 0;\nScene scene = {");
+	strcat(str, itoa0(x));
+	strcat(str, ",{");
+	rewinddir(dir);
+        while ((files = readdir(dir)) != NULL){
+                if(files->d_name[0] != '.' && strcmp(files->d_name, "allIncludes.h") && strcmp(files->d_name, "tfuncs.h")){
+			strcat(str, "wut(\"");
+			strcat(str, files->d_name);
+			strcat(str, "\"),");
+                }
+        }
+	str[strlen(str)-1] = 0;
+	strcat(str,"}};\n");
+        rewinddir(dir);
+        while ((files = readdir(dir)) != NULL){
+                if(files->d_name[0] != '.' && strcmp(files->d_name, "allIncludes.h") && strcmp(files->d_name, "tfuncs.h")){
+			strcat(str, files->d_name);
+                        strcat(str, "start(get_object(&scene, \"");
+                        strcat(str, files->d_name);
+                        strcat(str, "\"));\n");
+                }
+        }
+	rewinddir(dir);
+	strcat(str,"while (1){\nchar c = getch();\n");
+        while ((files = readdir(dir)) != NULL){
+                if(files->d_name[0] != '.' && strcmp(files->d_name, "allIncludes.h") && strcmp(files->d_name, "tfuncs.h")){
+                        strcat(str, files->d_name);
+                        strcat(str, "loop(get_object(&scene, \"");
+                        strcat(str, files->d_name);
+                        strcat(str, "\"),c);\n");
+                }
+        }
+	strcat(str,"Screen screen = sceneToScreen(scene);render(screen);freeScreen(screen);}}");
+        FILE *fp = fopen(CFile, "w");
+        if (fp == NULL)
+                return 0;
+        fprintf(fp, "%s\n", str);
+        fclose(fp);
+        free(files);
+        closedir(dir);
+}
+
+int uiStart(){
 	char c;
 	char buffer[40];
 	int i = 0;
@@ -174,92 +246,117 @@ int main(){
 				}
 			}
 			buffer[j] = '\0';
-			if(project[0] == 0 && (buffer[0]!='o' && buffer[0]!='q')){
+			if(project[0] == 0 && (buffer[0]!='o' && buffer[0]!='q' && (buffer[0]!='n' && buffer[1]!='p') && buffer[0]!='h')){
 				move(LINES-1,0);
 				printw("NO PROJECT OPENED");
 			}else{
-			move(0,0);
-			switch(buffer[0]){
-				case ('q'):
-					endwin();
-					exit(0);
-					break;
-				case ('n'):{
-					switch(buffer[1]){
-						case('o'):{
-							char **tdchar = splitBySpace(buffer);
-							move(LINES-1,0);
-							endwin();
-							char shit[30];
-							strcpy(shit, project);
-							strcat(shit, "/objects/");
-							strcat(shit, tdchar[1]);
-							strcpy(tdchar[1], shit);
-							start(4,tdchar);
-							initscr();
-							for(int i = 0; i<3; i++)
-								free(tdchar[i]);
-							break;
-						}case('p'):
-							break;
-						case('s'):{
-							char **tdchar = splitBySpace(buffer);
-							move(LINES-1,0);
-							char shit[30];
-							strcpy(shit, project);
-							strcat(shit, "/scripts/");
-							strcat(shit, tdchar[1]);
-							FILE *newScript = fopen(shit, "w");
-							char content[1024] = {0};
-							strcat(content, "int ");
-							strcat(content, tdchar[1]);
-							strcat(content, "start(){\n}\nint ");
-							strcat(content, tdchar[1]);
-							strcat(content, "loop(){\n}");
-							fprintf(newScript, "%s", content);
-							fclose(newScript);
-							for(int i = 0; i<2; i++)
-							        free(tdchar[i]);
-							genAllIncludes();
-							break;
-						}
+				move(0,0);
+				switch(buffer[0]){
+					case ('q'):
+						endwin();
+						exit(0);
+						break;
+					case ('c'):{
+						char command[100] = "gcc ";
+						strcat(command, project);
+						strcat(command, "/project.c -o ");
+						strcat(command, project);
+						strcat(command, "/bin -lncurses -ltinfo -lglut -lGL");
+						system(command);
+						move(LINES-2,0);
+						printw("RUN IN THE SAME DIRECTORY AS THE BINARY");
+						break;
+					} case ('h'):
+						move(LINES/2,0);
+						printw("Help Page - h\nfull controls available at the readme");
+						break;
+					case ('n'):{
+						switch(buffer[1]){
+							case('o'):{
+								char **tdchar = splitBySpace(buffer);
+								move(LINES-1,0);
+								endwin();
+								if(!(splitCount<4)){
+								char shit[30];
+								strcpy(shit, project);
+								strcat(shit, "/objects/");
+								strcat(shit, tdchar[1]);
+								strcpy(tdchar[1], shit);
+								start(4,tdchar);
+								}
+								initscr();
+								for(int i = 0; i<splitCount; i++)
+									free(tdchar[i]);
+								break;
+							}case('p'):{
+								char **tdchar = splitBySpace(buffer);
+								mkdir(tdchar[1],0777);
+								char command[50] = {0};
+								strcat(command, "cp -r tempProject/* ");
+								strcat(command, tdchar[1]);
+								system(command);
+								for(int i = 0; i<splitCount; i++)
+								        free(tdchar[i]);
+								break;
+							}case('s'):{
+								char **tdchar = splitBySpace(buffer);
+								move(LINES-1,0);
+								char shit[30];
+								strcpy(shit, project);
+								strcat(shit, "/scripts/");
+								strcat(shit, tdchar[1]);
+								FILE *newScript = fopen(shit, "w");
+								char content[1024] = {0};
+								strcat(content, "#include \"tfuncs.h\"\n#include \"../libs/object.h\"\nint ");
+								strcat(content, tdchar[1]);
+								strcat(content, "start(Object *self){\n}\nint ");
+								strcat(content, tdchar[1]);
+								strcat(content, "loop(Object *self, char c){\n}");
+								fprintf(newScript, "%s", content);
+								fclose(newScript);
+								for(int i = 0; i<splitCount; i++)
+									free(tdchar[i]);
+								genAllIncludes();
+								genCFile();
+								break;
+							}
 
-					}
-					break;
-				} case('p'):{
-					char **tdchar = splitBySpace(buffer);
-					Screen screen = initScreen();
-					char shit[30];
-					strcpy(shit, project);
-					strcat(shit, "/objects/");
-					strcat(shit, tdchar[1]);
-					readAndAdd(shit, &screen);
-					putchar('\n');
-					free(tdchar[0]);
-					free(tdchar[1]);
-					render(screen);
-					freeScreen(screen);
-					char cool;
-					while((cool = getch()) != '\n');
-					endwin();
-					initscr();
-					noecho();
-					cbreak();
-					break;
-				} case ('o'):{
-					char **tdchar = splitBySpace(buffer);
-					strcpy(project, tdchar[1]);
-					free(tdchar[0]);
-					free(tdchar[1]);
-					strcpy(projectFile, project);
-					strcat(projectFile, "/project.info");
-					break;
-				}default:
-					move(LINES-1,0);
-					printw("Unknown command: %c",buffer[0]);
-					break;
-			}
-			j=0;
+						}
+						break;
+					} case('p'):{
+						char **tdchar = splitBySpace(buffer);
+						Screen screen = initScreen();
+						char shit[30];
+						strcpy(shit, project);
+						strcat(shit, "/objects/");
+						strcat(shit, tdchar[1]);
+						readAndAdd(shit, &screen);
+						putchar('\n');
+						for(int i = 0; i<splitCount; i++)
+							free(tdchar[i]);
+						render(screen);
+						freeScreen(screen);
+						char cool;
+						while((cool = getch()) != '\n');
+						endwin();
+						initscr();
+						noecho();
+						cbreak();
+						break;
+					} case ('o'):{
+						char **tdchar = splitBySpace(buffer);
+						strcpy(project, tdchar[1]);
+						for(int i = 0; i<splitCount; i++)
+							free(tdchar[i]);
+						strcpy(projectFile, project);
+						strcat(projectFile, "/project.info");
+						break;
+					}default:
+						move(LINES-1,0);
+						printw("Unknown command: %c",buffer[0]);
+						break;
+				}
+				j=0;
 			}
 		}
 		i = 0;
--- include/object.h
+++ include/object.h
@@ -21,8 +21,55 @@ Object* get_object(Scene scene, char name[]){
 			return &scene.objects[i];
 }
 
-Object createObject(Scene *scene, char Polygons[]){
-	//
+Poly *readPolygons(char *fileName){ // read from a file polygon data
+	char contents[MAXSIZE], color;
+	Poly *polygons = malloc(40*sizeof(Polygon));
+	int currentVector[] = {0,0};
+	int tempCoord[3][2] = {{0,0},{0,0},{0,0}};
+	FILE *fptr;
+	if((fptr = fopen(fileName,"r")) == NULL){
+		printf("Error! opening file");
+		exit(1);
+	}
+	int curr = 0;
+	while (fscanf(fptr, "%s", contents) != EOF) { // welcome to hell
+		if (contents[0] > '9' || contents[0] < '0'){ // this means that the character is a "colour character" so it has reached the end of the polygon
+			color = contents[0];
+			polygons[curr++] = {(Poly) {{{tempCoord[0][0], tempCoord[0][1]},{tempCoord[1][0], tempCoord[1][1]},{tempCoord[2][0], tempCoord[2][1]}},color}}
+			currentVector[0] = 0; // reset the position where the polygon data is written to
+			currentVector[1] = 0;
+		}
+		else{
+			tempCoord[currentVector[1]][currentVector[0]] = atoi(contents); // the current string is probably an integer, just write to the current coordinate value
+			currentVector[0]++;
+		}
+		if(currentVector[0] > 1){ // if we have reached the end of the vertex, go onto the next field
+			currentVector[1]++;
+			currentVector[0] = 0;
+		}
+		if(currentVector[1] > 3){ // if it continues past the last position in tempCoord, just throw an error so it doesn't seg fault
+			printf("Error! Syntax Error: colour not specified or more than 3 vertecies specified");
+			exit(1);
+		}
+	}
+	fclose(fptr);
+	return readPolygons;
+}
+
+Object genObject(char dir[]){
+	char fileName[40] = {};
+	strcpy(fileName,"../objects/");
+	strcat(fileName,dir);
+	FILE *fp fopen(fileName,"r");;
+	char ch;
+	int lineCount = 0;
+	while((ch=fgetc(fp))!=EOF) {
+		if(ch=='\n')
+			linesCount++;
+	}
+	fclose(fp);
+	Object ret = {dir, 0, 0, readPolygon(fileName), lineCount};
+	return ret;
 }
 
 #endif
