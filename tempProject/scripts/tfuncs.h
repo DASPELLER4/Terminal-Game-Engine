1b6a35f10b47c4ab5c58a8cdf2dfe33ac14da8ac
DASPELLER4 <51001942+DASPELLER4@users.noreply.github.com>
2022-01-26 17:30:25 +0000

    Update tfuncs.h

--- tempProject/scripts/tfuncs.h
+++ tempProject/scripts/tfuncs.h
@@ -1,6 +1,11 @@
 #ifndef TFUNCS_H
 #define TFUNCS_H
 double sleeps[100];
+
+void sigintHandler(int sig_num){
+        signal(SIGINT, sigintHandler);
+}
+
 int tsleep(int index, double num){
         sleeps[index]+=0.00075;
         if(sleeps[index] < (double)num)
