70628adc0eecf2b6ef6f41e3e5cecf24e71298d8
DASPELLER4 <51001942+DASPELLER4@users.noreply.github.com>
2022-01-26 16:54:45 +0000

    Update tfuncs.h

--- tempProject/scripts/tfuncs.h
+++ tempProject/scripts/tfuncs.h
@@ -1,12 +1,21 @@
 #ifndef TFUNCS_H
 #define TFUNCS_H
 double sleeps[100];
-
 int tsleep(int index, int num){
         sleeps[index]+=0.00075;
         if(sleeps[index] < (double)num)
                 return 0;
         else
                 return (sleeps[index]=0)+1;
+}
+
+int kbhit(void){
+        int ch = getch();
+        if (ch != ERR) {
+                ungetch(ch);
+                return 1;
+        } else {
+                return 0;
+        }
 }
 #endif
